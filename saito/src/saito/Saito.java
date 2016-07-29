e830a1508aafb7a264d75c6dfdb0035ebd3d29d2
kensyu <kensyu@192.168.100.13>
2016-07-29 14:46:35 +0900

    GoodDay10回　初めのバージョン

---
+++ saito/src/saito/Saito.java
@@ -0,0 +1,17 @@
+package saito;
+
+public class Saito {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i = 0;
+		while(true){
+			System.out.println("GoodDay");
+		i++;
+		if(i == 10){
+			break;
+		}
+		}
+	}
+
+}
